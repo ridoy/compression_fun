5f51be87d4f2f19111ddd1bce4b6ee16402bdec8
ridoy <majumdar.ridoy@gmail.com>
2025-04-07 16:08:08 -0400

    bytes to bytes not str to bytes

--- shannon.py
+++ shannon.py
@@ -1,18 +1,18 @@
 # Implementation of Shannon Coding https://en.wikipedia.org/wiki/Shannon_coding
 import math
 import sys
+import random
 from dataclasses import dataclass
 
 @dataclass
 class Symbol:
-    char: str
+    byte: int
     count: int
-    strlen: int
+    num_bytes: int
     prob: float | None = None
     codeword_length: int | None = None
     cumprob: float | None = None
     codeword: str | None = None
-
 
 class FormatConverter:
     @staticmethod
@@ -34,35 +34,33 @@ class FormatConverter:
             x -= bit
             bits -= 1
         return result
-    
 
 class ShannonCoding:
-    def _compute_probs(self, symbols: dict[str,Symbol]) -> None:
+    def _compute_probs(self, symbols: dict[int,Symbol]) -> None:
         for symbol in symbols.values():
-            symbol.prob = symbol.count / symbol.strlen
+            symbol.prob = symbol.count / symbol.num_bytes
     def _codeword_length(self, p: float) -> int: return math.ceil(-1 * math.log2(p))
-    def _compute_codeword_lengths(self, symbols: dict[str,Symbol]) -> None:
+    def _compute_codeword_lengths(self, symbols: dict[int,Symbol]) -> None:
         for symbol in symbols.values():
             symbol.codeword_length = self._codeword_length(symbol.prob)
-    def _compute_cumprob(self, symbols: dict[str,Symbol]) -> None:
+    def _compute_cumprob(self, symbols: dict[int,Symbol]) -> None:
         prev = 0
         for symbol in sorted(symbols.values(), key=lambda x: x.codeword_length):
             symbol.cumprob = prev
             prev += symbol.prob
-    def _increment_bin_str(self, s: str) -> str: return "{0:b}".format(int(s, 2) + 1)
-    def _compute_encodings(self, symbols: dict[str,Symbol]) -> None:
+    def _compute_encodings(self, symbols: dict[int,Symbol]) -> None:
         for c, symbol in symbols.items():
             symbol.codeword = FormatConverter.dec_to_bin(symbol.cumprob, symbol.codeword_length)
-    def _encode(self, src: str, symbols: dict[str,Symbol]) -> tuple[bytes,int]:
-        return FormatConverter.bits_to_bytes("".join([symbols[c].codeword for c in src]))
-    def encode(self, src: str) -> tuple[bytes,dict[str, str]]:
-        symbols = { c: Symbol(c, src.count(c), len(src)) for c in set(src) }
+    def _encode(self, src: bytes, symbols: dict[str,Symbol]) -> tuple[bytes,int]:
+        return FormatConverter.bits_to_bytes("".join([symbols[b].codeword for b in src]))
+    def encode(self, src: bytes) -> tuple[bytes,int,dict[str, str]]:
+        symbols = { b: Symbol(byte=b, count=src.count(b), num_bytes=len(src)) for b in set(src) }
         self._compute_probs(symbols)
         self._compute_codeword_lengths(symbols)
         self._compute_cumprob(symbols)
         self._compute_encodings(symbols)
         encoded, padding = self._encode(src, symbols)
-        decodings = { symbol.codeword: symbol.char for symbol in symbols.values()}
+        decodings = { symbol.codeword: symbol.byte for symbol in symbols.values()}
         return (encoded, padding, decodings)
     def decode(self, b: bytes, padding: int, decodings: dict[str,str]) -> str:
         bitstr = FormatConverter.bytes_to_bits(b, padding)
@@ -73,13 +71,17 @@ class ShannonCoding:
             if buffer in decodings:
                 decoded.append(decodings[buffer])
                 buffer = ''
-        return ''.join(decoded)
+        return bytes(decoded)
 
 if __name__ == "__main__":
-    input_str = "this is an input string"
+    input_size = 1000
+    allowed = [ord('a'), ord('b'), ord('c')]
+    input_bytes = bytes(random.choice(allowed) for _ in range(input_size)) 
     encoder = ShannonCoding()
-    (encoded, padding, decodings) = encoder.encode(input_str)
-    print(f"Input str: {input_str}")
-    print(f"Encoded str: {encoded}")
-    print(f"Decoded str: {encoder.decode(encoded, padding, decodings)}")
+    (encoded, padding, decodings) = encoder.encode(input_bytes)
+    decoded = encoder.decode(encoded, padding, decodings)
+    print(f"Input length={len(input_bytes)} entropy={}")
+    print(f"Encoded bytes: {len(encoded)}")
+    print(f"Decoded bytes: {len(decoded)}")
+    print(f"input_bytes == decoded: {input_bytes == decoded}")
 
