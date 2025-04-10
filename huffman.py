8d4a5353a46a0a0bfeab769053e7ab84cc620764
ridoy <majumdar.ridoy@gmail.com>
2025-04-09 23:00:27 -0400

    complete huffman woo

--- huffman.py
+++ huffman.py
@@ -1,26 +1,51 @@
 import heapq
 import sys
 from dataclasses import dataclass
+from typing import Optional
 
-from helpers import compute_entropy, rand_input
+from helpers import FormatConverter, compute_entropy, rand_input
 
 @dataclass
 class TreeNode:
-  byte: int
-  left: TreeNode
-  right: TreeNode
+  byte: int | None
+  count: int
+  left: Optional["TreeNode"]
+  right: Optional["TreeNode"]
+  def __lt__(self, other):
+    return self.count < other.count
 
 class HuffmanCoding:
-  def _encode(self, src: bytes) -> bytes:
-    symbols = [(src.count(b), b) for b in set(src)]
+  def _encode(self, src: bytes) -> TreeNode:
+    symbols = [TreeNode(b, src.count(b), None, None) for b in set(src)]
     heapq.heapify(symbols)
-    while len(symbols) > 0:
-      print(heapq.heappop(symbols))
+    while len(symbols) > 1:
+      first, second = heapq.heappop(symbols), heapq.heappop(symbols)
+      new_node = TreeNode(None, first.count + second.count, first, second)
+      heapq.heappush(symbols,new_node)
+    root = heapq.heappop(symbols)
+    encodings = {}
+    self._compute_encodings(root, encodings, "")
+    decodings = {v: k for k,v in encodings.items()}
+    (encoded, padding) = FormatConverter.bits_to_bytes("".join([encodings[b] for b in src]))
+    return (encoded, padding, decodings)
+  def _compute_encodings(self, node: TreeNode, encodings: dict[int,str], code: str):
+    if node.left != None: self._compute_encodings(node.left, encodings, code + "0")
+    if node.right != None: self._compute_encodings(node.right, encodings, code + "1")
+    if node.left == None and node.right == None: encodings[node.byte] = code
+  def _decode(self, bitstr: str, decodings: dict[str,int]) -> bytes:
+      decoded = []
+      buffer = ''
+      for bit in bitstr:
+          buffer += bit
+          if buffer in decodings:
+              decoded.append(decodings[buffer])
+              buffer = ''
+      return bytes(decoded)
   def encode(self, src: bytes) -> bytes:
-    self._encode(src)
-    return (src, 0, {})
+    return self._encode(src)
   def decode(self, b: bytes, padding: int, decodings: dict[str,int]) -> bytes:
-    return b
+    bitstr = FormatConverter.bytes_to_bits(b, padding)
+    return self._decode(bitstr, decodings)
   
 if __name__ == "__main__":
     input_bytes = sys.argv[1].encode('utf-8') if len(sys.argv) > 1 else rand_input()
