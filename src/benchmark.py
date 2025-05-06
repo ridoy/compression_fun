3746b29a4ffc4af9bfd8da24770a1f83a0c0f671
ridoy <majumdar.ridoy@gmail.com>
2025-05-05 22:10:32 -0400

    consistent spacing

--- src/benchmark.py
+++ src/benchmark.py
@@ -7,34 +7,34 @@ from shannon import ShannonCoding
 
 # recursive list dir
 def listdir(path, level=1) -> list[str]:
-	files = os.listdir(path)
-	paths = []
-	for filename in files:
-		if filename == "SHA1SUM": continue
-		fullpath = f"{path}/{filename}"
-		if os.path.isdir(fullpath):
-			paths = paths + listdir(fullpath)
-		elif level != 0:
-			paths.append(fullpath)
-	return paths
+    files = os.listdir(path)
+    paths = []
+    for filename in files:
+        if filename == "SHA1SUM": continue
+        fullpath = f"{path}/{filename}"
+        if os.path.isdir(fullpath):
+            paths = paths + listdir(fullpath)
+        elif level != 0:
+            paths.append(fullpath)
+    return paths
 
 
 def evaluate_shannon(filepath: str):
-	encoder = ShannonCoding()
-	print(f"Evaluating on {filepath}...")
-	with open(filepath, 'rb') as f:
-		data = f.read()
-		(encoded, padding, decodings) = encoder.encode(data)
-		decoded = encoder.decode(encoded, padding, decodings)
-		msg = '\033[92msuccess\033[0m' if data == decoded else '\033[91mfailure\033[0m'
-		print(msg, end=" ")
-		print(f"with compression ratio: {len(data)/len(encoded)}")
+    encoder = ShannonCoding()
+    print(f"Evaluating on {filepath}...")
+    with open(filepath, 'rb') as f:
+        data = f.read()
+        (encoded, padding, decodings) = encoder.encode(data)
+        decoded = encoder.decode(encoded, padding, decodings)
+        msg = '\033[92msuccess\033[0m' if data == decoded else '\033[91mfailure\033[0m'
+        print(msg, end=" ")
+        print(f"with compression ratio: {len(data)/len(encoded)}")
 
 
 if __name__ == "__main__":
-	if len(sys.argv) > 1:
-		evaluate_shannon(sys.argv[1])
-	else:
-		filepaths = listdir('canterbury-corpus', level=0)
-		for filepath in filepaths:
-			evaluate_shannon(filepath)
+    if len(sys.argv) > 1:
+        evaluate_shannon(sys.argv[1])
+    else:
+        filepaths = listdir('canterbury-corpus', level=0)
+        for filepath in filepaths:
+            evaluate_shannon(filepath)
--- src/lempelziv.py
+++ src/lempelziv.py
@@ -3,93 +3,93 @@ import math
 from helpers import FormatConverter, debug
 
 class LZW:
-  def _encode(self, src: bytes) -> tuple[bytes,int]:
-    size = 256
-    dictionary = {bytes([i]): i for i in range(256)}
-    curr = b""
-    codes = []
-    for byte in src:
-      if (next := curr + bytes([byte])) in dictionary:
-        curr = next
-      else:
-        codes.append(dictionary[curr])
-        dictionary[next] = size
-        size += 1
-        curr = bytes([byte])
-    if curr:
-      codes.append(dictionary[curr])
-    debug(dictionary)
-    bitwidth = max(9, math.ceil(math.log2(size + 1)))
-    debug(bitwidth)
-    bitstr = "".join([format(code, f'0{bitwidth}b') for code in codes])
-    debug(" ".join([format(code, f'0{bitwidth}b') for code in codes]))
-    return (bitstr, bitwidth)
+    def _encode(self, src: bytes) -> tuple[bytes,int]:
+        size = 256
+        dictionary = {bytes([i]): i for i in range(256)}
+        curr = b""
+        codes = []
+        for byte in src:
+            if (next := curr + bytes([byte])) in dictionary:
+                curr = next
+            else:
+                codes.append(dictionary[curr])
+                dictionary[next] = size
+                size += 1
+                curr = bytes([byte])
+        if curr:
+            codes.append(dictionary[curr])
+        debug(dictionary)
+        bitwidth = max(9, math.ceil(math.log2(size + 1)))
+        debug(bitwidth)
+        bitstr = "".join([format(code, f'0{bitwidth}b') for code in codes])
+        debug(" ".join([format(code, f'0{bitwidth}b') for code in codes]))
+        return (bitstr, bitwidth)
 
-  def encode(self, src: bytes) -> tuple[bytes,int,int]:
-    (bitstr, bitwidth) = self._encode(src)
-    (encoded, padding) = FormatConverter.bits_to_bytes(bitstr)
-    return (encoded, padding, bitwidth)
+    def encode(self, src: bytes) -> tuple[bytes,int,int]:
+        (bitstr, bitwidth) = self._encode(src)
+        (encoded, padding) = FormatConverter.bits_to_bytes(bitstr)
+        return (encoded, padding, bitwidth)
 
-  def decode(self, encoded: bytes, padding: int, bitwidth: int) -> bytes:
-    bitstr = FormatConverter.bytes_to_bits(encoded, padding)
+    def decode(self, encoded: bytes, padding: int, bitwidth: int) -> bytes:
+        bitstr = FormatConverter.bytes_to_bits(encoded, padding)
 
-    size = 256
-    dictionary = {bytes([i]): i for i in range(256)}
-    curr = b""
-    codes = []
-    for byte in src:
-      if (next := curr + bytes([byte])) in dictionary:
-        curr = next
-      else:
-        codes.append(dictionary[curr])
-        dictionary[next] = size
-        size += 1
-        curr = bytes([byte])
-    if curr:
-      codes.append(dictionary[curr])
-    bitwidth = max(9, math.ceil(math.log2(size + 1)))
-    debug(bitwidth)
-    bitstr = "".join([format(code, f'0{bitwidth}b') for code in codes])
-    debug(",".join([format(code, f'0{bitwidth}b') for code in codes]))
+        size = 256
+        dictionary = {bytes([i]): i for i in range(256)}
+        curr = b""
+        codes = []
+        for byte in src:
+            if (next := curr + bytes([byte])) in dictionary:
+                curr = next
+            else:
+                codes.append(dictionary[curr])
+                dictionary[next] = size
+                size += 1
+                curr = bytes([byte])
+        if curr:
+            codes.append(dictionary[curr])
+        bitwidth = max(9, math.ceil(math.log2(size + 1)))
+        debug(bitwidth)
+        bitstr = "".join([format(code, f'0{bitwidth}b') for code in codes])
+        debug(",".join([format(code, f'0{bitwidth}b') for code in codes]))
 
-    return (bitstr, bitwidth)
+        return (bitstr, bitwidth)
 
 class LZW2:
-  def _encode(self, src: bytes, init_size: int = 256) -> tuple[bytes,int]:
-    size, curr, codes = 256, b"", []
-    dictionary = {bytes([i]): i for i in range(init_size)}
-    for byte in src:
-      if (next := curr + bytes([byte])) in dictionary:
-        curr = next
-      else:
-        codes.append(dictionary[curr])
-        dictionary[next] = size
-        size += 1
-        curr = bytes([byte])
-    if curr:
-      codes.append(dictionary[curr])
-    bitwidth = max(9, math.ceil(math.log2(size + 1)))
-    extended_bitwidth = math.floor(math.log2(size - init_size)) + 1
-    bitstr = ""
-    mask = ~(1 << bitwidth - 1)
-    for code in codes:
-      if code >> bitwidth - 1 == 1: # is extended, shave off some bits
-        debug("1" + format(code & mask, f'0{extended_bitwidth}b'), end=" ")
-        bitstr += "1" + format(code & mask, f'0{extended_bitwidth}b')
-      else:
-        debug(format(code, f'0{bitwidth}b'), end=" ")
-        bitstr += format(code, f'0{bitwidth}b')
-    return (bitstr, bitwidth, extended_bitwidth)
+    def _encode(self, src: bytes, init_size: int = 256) -> tuple[bytes,int]:
+        size, curr, codes = 256, b"", []
+        dictionary = {bytes([i]): i for i in range(init_size)}
+        for byte in src:
+            if (next := curr + bytes([byte])) in dictionary:
+                curr = next
+            else:
+                codes.append(dictionary[curr])
+                dictionary[next] = size
+                size += 1
+                curr = bytes([byte])
+        if curr:
+            codes.append(dictionary[curr])
+        bitwidth = max(9, math.ceil(math.log2(size + 1)))
+        extended_bitwidth = math.floor(math.log2(size - init_size)) + 1
+        bitstr = ""
+        mask = ~(1 << bitwidth - 1)
+        for code in codes:
+            if code >> bitwidth - 1 == 1: # is extended, shave off some bits
+                debug("1" + format(code & mask, f'0{extended_bitwidth}b'), end=" ")
+                bitstr += "1" + format(code & mask, f'0{extended_bitwidth}b')
+            else:
+                debug(format(code, f'0{bitwidth}b'), end=" ")
+                bitstr += format(code, f'0{bitwidth}b')
+        return (bitstr, bitwidth, extended_bitwidth)
 
-  def encode(self, src: bytes) -> tuple[bytes,int]:
-    (bitstr, bitwidth, extended_bitwidth) = self._encode(src)
-    (encoded, padding) = FormatConverter.bits_to_bytes(bitstr)
-    return (encoded, padding, bitwidth, extended_bitwidth)
+    def encode(self, src: bytes) -> tuple[bytes,int]:
+        (bitstr, bitwidth, extended_bitwidth) = self._encode(src)
+        (encoded, padding) = FormatConverter.bits_to_bytes(bitstr)
+        return (encoded, padding, bitwidth, extended_bitwidth)
 
 
 if __name__ == "__main__":
-  encoder = LZW()
-  encoder2 = LZW2()
-  s = b"ABABABCABABABCABABABCABABABCABABABC"
-  (encoded, padding, bitwidth) = encoder.encode(s)
-  (encoded, padding, bitwidth, extended_bitwidth) = encoder2.encode(s)
+    encoder = LZW()
+    encoder2 = LZW2()
+    s = b"ABABABCABABABCABABABCABABABCABABABC"
+    (encoded, padding, bitwidth) = encoder.encode(s)
+    (encoded, padding, bitwidth, extended_bitwidth) = encoder2.encode(s)
